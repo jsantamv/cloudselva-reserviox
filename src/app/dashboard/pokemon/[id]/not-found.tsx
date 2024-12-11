5ec2609205c3e64a6d79e56f67a1d029e482dec3
Juan Carlos Santamaria V <jsantamv@gmail.com>
2024-12-10 20:38:08 -0600

    update listado

--- src/app/dashboard/pokemon/[id]/not-found.tsx
+++ src/app/dashboard/pokemon/[id]/not-found.tsx
@@ -22,7 +22,7 @@ export default function NotFound() {
                 ></span>
 
                 <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-                  <Link href="/dashboard/main">Go Home</Link>
+                  <Link href="/dashboard/pokemon">ver listado de pokes</Link>
                 </span>
               </div>
             </button>
